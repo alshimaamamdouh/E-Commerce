ab270f68c0ba22b01f9b744d788a8f0adafe9094
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-08-13 22:35:12 +0300

    13/8/2024

--- myEcommerceEnv/ECommerce/backend/urls.py
+++ myEcommerceEnv/ECommerce/backend/urls.py
@@ -23,27 +23,26 @@ cart_update = CartViewSet.as_view({'put': 'update_cart'})
 cart_remove = CartViewSet.as_view({'delete': 'remove_from_cart'})
 
 urlpatterns = [
-    path('', include(router.urls)),  # Automatically includes all router URLs
-    path('index/', index, name='index'),
+    path('', include(router.urls)),  
     path('cart/<int:user_id>/', cart_list, name='cart-list'),
     path('cart/<int:user_id>/add/', cart_add, name='cart-add'),
     path('cart/<int:user_id>/update/', cart_update, name='cart-update'),
     path('cart/<int:user_id>/remove/<int:product_id>/', cart_remove, name='cart-remove'),
     
-    # Specific actions for products and reviews
+    # Product 
     path('products/add/', ProductViewSet.as_view({'post': 'create'}), name='product-create'),
     
     
-    # User management
+    # User 
     path('users/<int:pk>/update/', UserViewSet.as_view({'put': 'update'}), name='user-update'),
     path('users/<int:pk>/delete/', UserViewSet.as_view({'delete': 'destroy'}), name='user-delete'),
     
-    # Review management
+    # Review 
     path('reviews/add/', ReviewViewSet.as_view({'post': 'create'}), name='review-create'),
     path('reviews/<int:pk>/update/', ReviewViewSet.as_view({'put': 'update'}), name='review-update'),
     path('reviews/<int:pk>/delete/', ReviewViewSet.as_view({'delete': 'destroy'}), name='review-delete'),
     
-    # Category management
+    # Category 
     path('categories/<int:pk>/update/', CategoryViewSet.as_view({'put': 'update'}), name='category-update'),
     path('categories/<int:pk>/delete/', CategoryViewSet.as_view({'delete': 'destroy'}), name='category-delete'),
 ]
